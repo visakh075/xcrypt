0fe6be5930969fea3a8c9f0c9007c3ef0fb4f307
visakh075 <visakh075@gmail.com>
2020-10-08 15:29:27 +0530

    skelton crypt

--- lib/libcrypt.c
+++ lib/libcrypt.c
@@ -1,9 +1,10 @@
 #include "libcrypt.h"
 #include <stdio.h>
 size_t len(char *in){size_t m=0;while(1){if(in[m]!='\0')m++;else break;}return(m);}
-void * crypt(void * args)
+void * crypt(void * data)
 {
-    crypt_args * args_in;
-    args_in=(crypt_args *)args;
-    printf("i/p: %s\no/p:%s",args_in->in,args_in->out);
+    crypt_args * args;
+    args=(crypt_args *)data;
+    printf("i/p: %s\no/p:%s\n",args->in,args->out);
+    return(NULL);
 }
--- xcrypt.c
+++ xcrypt.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "lib/libcrypt.h"
-crypt_args args;
+
 int main(int argc,char ** argv)
 {
+crypt_args args;
 args.in="thermit";
 args.out="podohey";
 crypt((void *)&args);
+return(0);
 }
